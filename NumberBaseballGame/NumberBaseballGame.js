c8974e26ab4b17b42b58ffabb7f8c71d358c53c2
KimSatGat <cbaal@korea.com>
2020-06-24 16:19:36 +0900

    Add Comments

--- NumberBaseballGame/NumberBaseballGame.js
+++ NumberBaseballGame/NumberBaseballGame.js
@@ -2,10 +2,11 @@ var result = document.querySelector("#result");
 result.textContent = "정답을 입력하세요.";
 var form = document.querySelector("#form");
 var input = document.querySelector("#input");
-var numberList;
-var selectedNumbers;
-var failCount = 0;
-function selectNumbers(){
+var numberList;         // 숫자 후보군
+var selectedNumbers;    // 선택한 숫자를 담을 배열
+var failCount = 0;      // 실패 카운트
+
+function selectNumbers(){   // 랜덤 숫자 뽑기
     numberList = [1,2,3,4,5,6,7,8,9];
     selectedNumbers = [];
     for(var i = 0; i < 4; i++){
@@ -15,11 +16,10 @@ function selectNumbers(){
 }
 
 selectNumbers();
-console.log(selectedNumbers);
 form.addEventListener('submit', function action(event){
-    event.preventDefault();
+    event.preventDefault();     // 새로고침 막기
     var answer = input.value;    
-    if(answer === selectedNumbers.join('')){
+    if(answer === selectedNumbers.join('')){    // 정답이라면
         result.textContent = "홈런!";
         input.value = '';
         input.focus();
@@ -30,13 +30,13 @@ form.addEventListener('submit', function action(event){
         var strike = 0;
         var ball = 0;
         ++failCount;
-        if(failCount > 3){
+        if(failCount > 3){  // 정답이 아니고 기회를 초과하면
             result.textContent = '3번 넘게 틀려서 실패! 답은 ' + selectedNumbers.join(',') + '입니다.';
             input.value = '';
             input.focus();
             selectNumbers();
             failCount = 0;
-        }else{
+        }else{              // 정답이 아니고 기회 이하라면
             for(var i = 0; i < 4; ++i){
                 if(Number(answerArray[i]) === selectedNumbers[i]) ++strike;
                 else if(selectedNumbers.indexOf(Number(answerArray[i])) > -1) ++ball;
